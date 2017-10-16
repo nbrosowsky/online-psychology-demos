9453acbe88f484492647b30a9311076165f933f5
Nicholaus P. Brosowsky <nbrosowsky@gmail.com>
2017-10-16 19:46:19 -0400

    clean up

--- TaskSwitching/js/task.js
+++ TaskSwitching/js/task.js
@@ -291,65 +291,6 @@ function summarizeData() {
 
 ////////////// Set up initial display & button functions //////////////////////////////////////
 /* These functions need to run after document has loaded */
-var pagination = {
-    currentPage: "",
-    firstPage: "",
-    lastPage: "",
-    ids: [],
-
-
-    setup: function (pageOrderArray) {
-        if (typeof pageOrderArray === 'undefined') {
-            var el = document.querySelectorAll(".book .page");
-            for (i = 0; i <= el.length - 1; i++) {
-                this.ids.push(el[i].id)
-            };
-        } else {
-            this.ids = pageOrderArray;
-        }
-
-        this.currentPage = 0;
-        this.firstPage = 0;
-        this.lastPage = this.ids.length - 1;
-
-        $("#" + this.ids[0]).show();
-
-        for (i = 1; i <= this.ids.length; i++) {
-            $("#" + this.ids[i]).hide();
-        }
-
-        $("#pageUp").click(function () {
-            pagination.pageUp();
-        });
-        $("#pageDown").click(function () {
-            pagination.pageDown();
-        });
-
-    },
-
-    pageUp: function () {
-        $("#pageDown").show();
-        $("#" + this.ids[this.currentPage]).hide();
-        pagination.currentPage++;
-        $("#" + pagination.ids[this.currentPage]).show();
-        if (this.currentPage === this.lastPage) {
-            $("#pageUp").hide();
-            $("#beginExp").show();
-        }
-    },
-
-    pageDown: function () {
-        $("#" + this.ids[this.currentPage]).hide();
-        $("#beginExp").hide();
-        this.currentPage--;
-        $("#" + this.ids[this.currentPage]).show();
-        $("#pageUp").show();
-        if (this.currentPage === this.firstPage) {
-            $("#pageDown").hide();
-        }
-    }
-}
-// pages = ["pg5","pg3","pg1","pg4"]
 pagination.setup();
 
 
